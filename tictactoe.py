8c8db78437ce8b832318b5e56301d8ced024e795
pstevenson28 <pearsestevenson@gmail.com>
2021-04-02 10:34:10 +0000

    change to input

--- tictactoe.py
+++ tictactoe.py
@@ -10,11 +10,11 @@ def spaceIsFree(pos):
 
 def printBoard(board):
     
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('-----------')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('-----------')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     
     
 def isWinner(bo, le):
